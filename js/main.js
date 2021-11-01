a5dcc8329ef21cdd11649d5c6f30550f41603ee0
KamilMinikeev <60263392+KamilMinikeev@users.noreply.github.com>
2021-11-01 18:44:44 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -120,32 +120,5 @@ $(function () {
         }
     })
 
-    //webp
-
-    window.onload = function () {
-        // Получаем все элементы с дата-атрибутом data-bg
-        let images = document.querySelectorAll('[data-bg]');
-        // Проходимся по каждому
-        for (let i = 0; i < images.length; i++) {
-            // Получаем значение каждого дата-атрибута
-            let image = images[i].getAttribute('data-bg');
-            // Каждому найденному элементу задаем свойство background-image с изображение формата jpg
-            images[i].style.backgroundImage = 'url(' + image + ')';
-        }
-
-        // Проверяем, является ли браузер посетителя сайта Firefox и получаем его версию
-        let isitFirefox = window.navigator.userAgent.match(/Firefox\/([0-9]+)\./);
-        let firefoxVer = isitFirefox ? parseInt(isitFirefox[1]) : 0;
-
-        // Если есть поддержка Webp или браузер Firefox версии больше или равно 65
-        if (canUseWebp() || firefoxVer >= 65) {
-            // Делаем все то же самое что и для jpg, но уже для изображений формата Webp
-            let imagesWebp = document.querySelectorAll('[data-bg-webp]');
-            for (let i = 0; i < imagesWebp.length; i++) {
-                let imageWebp = imagesWebp[i].getAttribute('data-bg-webp');
-                imagesWebp[i].style.backgroundImage = 'url(' + imageWebp + ')';
-            }
-        }
-    };
 
 })
